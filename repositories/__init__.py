5c5798ac69081d9bace7a4c4295a0a9014ad212f
malekazmy00 <malekazmy00@gmail.com>
2026-07-25 16:13:01 +0300

    Update __init__.py

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,5 +1,6 @@
 # repositories/__init__.py
-from repositories.base import BaseRepository, UnitOfWork
+from repositories.base import BaseRepository
+from core.database import UnitOfWork
 from repositories.user_repo import user_repo, UserRepository
 from repositories.item_repo import item_repo, ItemRepository
 from repositories.log_repo import log_repo, LogRepository
